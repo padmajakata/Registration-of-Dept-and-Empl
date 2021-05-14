23686e93930bffd6a2e8902539bf3a73ba64e8ad
padmajakata <83570640+padmajakata@users.noreply.github.com>
2021-05-14 11:21:04 +0530

    Add files via upload

---
+++ review/CurdexmpApplication.java
@@ -0,0 +1,13 @@
+package com.Curdexmp.Curdexmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdexmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdexmpApplication.class, args);
+	}
+
+}
